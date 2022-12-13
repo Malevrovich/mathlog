478369617c2779c2bd4e1050e4d5541329e0dc5b
malevrovich <malevrovich@LAPTOP-4QT8RMP5.localdomain>
2022-12-13 23:33:29 +0300

    test utils added

---
+++ task1/tests/test_utils.h
@@ -0,0 +1,18 @@
+#ifndef _TEST_UTILS_H_
+#define _TEST_UTILS_H_
+
+#include <string.h>
+#include <stdio.h>
+
+#define TEST(name) void test_##name()
+
+#define RUN_TEST(name)                      \
+do {                                        \
+    printf("------------------------\n");   \
+    printf("Running test " #name "...\n");     \
+    test_##name();                          \
+    printf("Success!\n");                    \
+    printf("------------------------\n");   \
+} while(0)
+
+#endif
