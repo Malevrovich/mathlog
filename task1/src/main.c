16cbfcceffe3c870b62f5d1c90e67fce29dce0c5
malevrovich <malevrovich@LAPTOP-4QT8RMP5>
2023-01-24 15:39:45 +0300

    main.c added

--- task1/src/main.c
+++ task1/src/main.c
@@ -1,5 +1,50 @@
+#include "tokenize.h"
+#include "ast.h"
+#include "ast_parse.h"
+#include "ast_debug.h"
+#include "tokenize_debug.h"
+
 #include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE (1024 * 100 + 1)
+
+DEFINE_LIST(token, struct token);
 
 int main() {
-    printf("Main\n");
+    char *buf = calloc(BUFFER_SIZE, sizeof(char));
+    
+    if(!fgets(buf, BUFFER_SIZE, stdin)) {
+        fprintf(stderr, "Unable to read input line\n");
+        free(buf);
+        return 1;
+    }
+
+    struct list_token **tokens = tokenize(buf);
+
+    if(!tokens) {
+        fprintf(stderr, "Tokenization error\n");
+        free(buf);
+        return 1;
+    }
+
+
+
+    struct parse_ast_res parse_res = parse_ast(tokens);
+
+    if(parse_res.status != PARSE_AST_SUCCESS) {
+        fprintf(stderr, "Parsing failed\n");
+        fprintf(stderr, "%s\n", PARSE_AST_RES_STRING[parse_res.status]);
+
+        free(buf);
+        list_token_free(tokens);
+        free(tokens);
+
+        return 1;
+    }
+
+    print_ast(stdout, parse_res.val);
+    printf("\n");
+
+    return 0;
 }
